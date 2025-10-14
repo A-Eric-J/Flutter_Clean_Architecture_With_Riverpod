3820705dd7201947cbf0ca73e2101e7a2866de73
amiirrallii <amirali007z@yahoo.com>
2025-10-14 14:59:53 +0330

    Flutter Gen created.

--- lib/features/login/presentation/ui/widget/login_with.dart
+++ lib/features/login/presentation/ui/widget/login_with.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture_with_riverpod/common/extention/string_hardcoded.dart';
+import 'package:flutter_clean_architecture_with_riverpod/gen/assets.gen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginWith extends ConsumerWidget {
@@ -13,13 +14,19 @@ class LoginWith extends ConsumerWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           IconButton(
-            icon: const Icon(Icons.android, color: Colors.blue),
+            icon:  Assets.images.google.svg(
+              width: 24,
+              height: 24
+            ),
             onPressed: () {
               // Handle Google login
             },
           ),
           IconButton(
-            icon: const Icon(Icons.apple, color: Colors.black),
+            icon: Assets.images.apple.svg(
+                width: 24,
+                height: 24
+            ),
             onPressed: () {
               // Handle Apple login
             },
